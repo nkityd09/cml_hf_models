c3b0aadfe129be6a9f57d353134829bc6f576376
nkityd09 <ankity@cloudera.com>
2023-11-20 13:44:54 -0800

    Update UI

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # CML HuggingFace Models
 
 Deploy HuggingFace Models as Cloudera Machine Learning Models. Cloudera Machine Learning models expose an Inference endpoint for users to access and communicate with.
+
+## Key Features
+- Launch any 7B or 13B HuggingFace Model(Can be configured for larger models after deployment)
+- Deploys CML Model with 1 replica
+- Deploys CML Gradio Application which interacts with deployed CML Model
+
+## Resource Requirements
+The AMP Model has been configured to use the following
+- 4 CPU
+- 32 GB RAM
+- 1 GPUs
+
+## Steps to Configure CML AMP
+1. Navigate to CML Workspace -> Site Administration -> AMPs Tab
+
+2. Under AMP Catalog Sources section, We will "Add a new source By" selecting "Catalog File URL"
+
+3. Provide the following URL and click "Add Source"
+```
+https://raw.githubusercontent.com/nkityd09/cml_speech_to_text/main/catalog.yaml
+```
+
+4. Once added, We will be able to see the LLM PDF Document Chatbot in the AMP section and deploy it from there.
+
+5. Click on the AMP and "Configure Project"
+   - Add HuggingFace Model Name, defaults to HuggingFaceH4/zephyr-7b-alpha
+   - If accessing a gated model, add HuggingFace token. Can be left blank for non gated models
+![Configuring AMP](images/configuring_amp.png)
+
+6. Once the AMP steps are completed, We can access the Gradio UI via the Applications page.
+
+## CML Application in Action
+
+
--- app/gradio_app.py
+++ app/gradio_app.py
@@ -9,28 +9,29 @@ def generate_response(question):
     DOMAIN = os.environ["CDSW_DOMAIN"]
     API_KEY = os.environ["CDSW_APIV2_KEY"]
     PROJECT_ID = os.environ["CDSW_PROJECT_ID"]
-    
+    HEADERS = {'Content-Type': 'application/json'}
     API_URL = f'https://modelservice.{DOMAIN}/model'
     WORKSPACE_DOMAIN = f"https://{DOMAIN}"
     CML_CLIENT = cmlapi.default_client(WORKSPACE_DOMAIN, API_KEY)
     models = CML_CLIENT.list_models(PROJECT_ID, search_filter=json.dumps({"name": "LLM_Model"}))
     MODEL_ACCESS_KEY = models.models[0].access_key    
-    HEADERS = {'Content-Type': 'application/json'}
+    
     question = {'question': question}
     data = json.dumps({'accessKey': MODEL_ACCESS_KEY, 'request': question})
     response = requests.post(API_URL, data = data, headers = HEADERS)
     try:
         response_json = response.json()
-        print(response_json.get('response'))
         return response_json.get('response')
     except json.JSONDecodeError:
         return "Failed to parse JSON response"
     
 with gr.Blocks() as demo:
-    gr.Markdown("# CML LLM Models ")
+    HF_MODEL=os.environ["HF_MODEL"]
+    gr.Markdown(f"# {HF_MODEL} CML Model ")
+    gr.Markdown(f'Interact with {HF_MODEL} deplyed as a CML Model')
     with gr.Row():
-        input = gr.Textbox("Question")
-        output = gr.Textbox()
+        input = gr.Textbox("Question",label= "Question", show_copy_button=True)
+        output = gr.Textbox(label="Answer", show_copy_button=True)
     submit_btn = gr.Button("Submit")
     submit_btn.click(fn=generate_response, inputs=input, outputs=output)
 
